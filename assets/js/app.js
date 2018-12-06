bcc7dc0143ce17d31c5320f6c5947423835689c0
Frederic LAINE <frederic.laine@eggers-digital.com>
2018-12-06 15:03:50 +0100

    Merge-Passage à webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,19 @@ require('../scss/global.scss');
 
 global.$ = global.jQuery = require('jquery');
 
-require('bootstrap');
+require("bootstrap");
+require("datatables.net");
 require("datatables.net-bs4");
+require('datatables.net-buttons');
 require("datatables.net-buttons-bs4");
+require('datatables.net-select');
 require("datatables.net-select-bs4");
+require("datatables.net-rowreorder");
+require("jquery-sortable");
+require('jquery-ui');
+require('jquery-ui/ui/widgets/sortable');
+require('jquery-ui/ui/disable-selection');
+
 
 const Swal = require('sweetalert2');
 global.Swal = Swal;
