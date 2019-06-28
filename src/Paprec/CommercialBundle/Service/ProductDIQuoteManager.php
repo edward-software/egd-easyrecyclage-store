e66f35f2e757f0ce30f9c7724ddae5acf0eec257
Frederic Laine <frederic.laine@eggers-digital.com>
2019-06-28 16:46:54 +0200

    [EDIT] Devis DI

--- src/Paprec/CommercialBundle/Service/ProductDIQuoteManager.php
+++ src/Paprec/CommercialBundle/Service/ProductDIQuoteManager.php
@@ -333,8 +333,6 @@ class ProductDIQuoteManager
             $snappy = new Pdf($this->container->getParameter('wkhtmltopdf_path'));
             $snappy->setOption('margin-left', 3);
             $snappy->setOption('margin-right', 3);
-            $snappy->setOption('page-size', 'A4');
-            $snappy->setOption('dpi', '72');
 
             /**
              * On génère les PDF
